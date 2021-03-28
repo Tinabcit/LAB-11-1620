2fea7f9559237d43b2e3f0f0f0c34b440adf16ef
Tina <tshabestari@my.bcit.ca>
2021-03-27 19:42:59 -0700

    Add a p4 of javascript which create todoList array object which included todo and status keys which shows one started or completed form. Secondly add a second empty array for completedTodos list.

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,11 @@
-// your JavaScript goes here, you can delete this comment
+// p4 part Create arrays for the lab first part in the javascript
+const todoList = {
+    todo: "clone repo for starter code", status: "completed",
+}
+console.log(todoList)
+
+const secondtodoList = {
+    secondtodo: "Linking HTML file to the Css file", status:"started",
+}
+console.log(secondtodoList)
+
